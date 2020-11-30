908a9e371290b80df105a3bf33960f76ada7f2e7
HaNaK0 <saturn-v@hotmail.com>
2020-11-30 15:27:27 +0100

    Window created

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use amethyst::{Application, GameDataBuilder, utils::{
-	application_dir, 
+use amethyst::{Application, GameDataBuilder, renderer::{RenderingBundle, types::DefaultBackend, RenderToWindow}, utils::{
 	application_root_dir,
 }};
 
@@ -12,7 +11,14 @@ fn main() -> amethyst::Result<()> {
 	let app_root = application_root_dir()?; 
 	let display_config_path = app_root.join("config").join("display.ron");
 
-	let game_data = GameDataBuilder::default();
+	let game_data = GameDataBuilder::default()
+		.with_bundle(
+			RenderingBundle::<DefaultBackend>::new()
+				.with_plugin(
+					RenderToWindow::from_config_path(display_config_path)?
+						.with_clear([0.0, 0.0, 0.0, 1.0])
+				)
+		)?;
 
 	let asset_dir = app_root.join("assets");
 	let mut game = Application::new(
--- src/minesweeper.rs
+++ src/minesweeper.rs
@@ -10,8 +10,4 @@ impl SimpleState for GameState {
 	fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {
 		println!("Hello world");
 	}
-
-	fn update(&mut self, _data: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
-		return Trans::Quit;
-	}
 }
